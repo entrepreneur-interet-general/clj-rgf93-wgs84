702e9bb490c5bdcef674aee3c820162dc94bf01b
Bastien <bzg@gnu.org>
2017-11-24 08:24:22 +0100

    Use Apache 2.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-rgf93-wgs84 "0.1.0-SNAPSHOT"
   :description "Convert RGF93 coordinates to WGS84"
   :url "https://git.inventaire-des-orgues.fr/bzg/clj-rgf93-wgs84"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache 2.0"
+            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[lein-codox "0.10.3"]
             [lein-kibit "0.1.3"]])
